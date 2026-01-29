8108306fd32786357857404a818de0149767ab77
SaidaAourras <sa123aouras@gmail.com>
2026-01-29 21:12:53 +0100

    fichiers des fonction logic metier des routes

---
+++ src/services/auth_services.py
@@ -0,0 +1,43 @@
+from jose import jwt , JWTError
+from dotenv import load_dotenv
+from src.db.models.user import User
+from datetime import datetime , timedelta
+import os
+from fastapi.security import HTTPAuthorizationCredentials , HTTPBearer
+from fastapi import Depends , HTTPException , status
+from src.api.dependencies import get_db
+load_dotenv()
+
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+JWT_EXPIRATION_MINUTES = int(os.getenv('JWT_EXPIRATION_MINUTES',30))
+
+bearer_scheme = HTTPBearer()
+
+
+
+# create token
+def create_token(payload):
+    
+    to_encode = payload.copy()
+    expire = datetime.utcnow() + timedelta(minutes=JWT_EXPIRATION_MINUTES)
+    
+    to_encode.update({
+        "exp":expire,
+        "iat":datetime.utcnow()
+    })
+    
+    token = jwt.encode(to_encode , JWT_SECRET_KEY)
+    return token
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme), db=Depends(get_db)):
+    token = credentials.credentials
+    
+    try:
+        payload = jwt.decode(token , JWT_SECRET_KEY )
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="token invalide ou expire",
+        )
