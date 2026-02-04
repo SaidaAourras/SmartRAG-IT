08ebb0b4974701f97af6757acc366f2f592ae951
SaidaAourras <sa123aouras@gmail.com>
2026-02-04 22:24:37 +0100

    feat: integrate new routers (query, history, health)

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from src.db.database import engine
 from src.db.models.base import Base
 from src.api.routes.auth import auth_router
+from src.api.routes.query import query_router
+from src.api.routes.history import query_history_router
 
 
 
@@ -12,3 +14,5 @@ app = FastAPI()
 Base.metadata.create_all(engine)
 
 app.include_router(auth_router , prefix='/api/v1')
+app.include_router(query_router , prefix='/api/v1')
+app.include_router(query_history_router , prefix='/api/v1')
